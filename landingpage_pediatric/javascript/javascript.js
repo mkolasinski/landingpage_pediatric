a19bbe45debd7617205a8d31789ca9268a937b00
mkolasinski <m.kolasinski@poczta.fm>
2018-09-19 19:09:09 +0200

    Next sections and styles

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -1,5 +1,17 @@
 $(document).ready( function() {
 
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 650);
+        }
+    });
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2 ) {
           $(".navigation-top").addClass("navigation-top-scrolled");
