3d142124582875fa24210ae39074954f8105816e
mkolasinski <m.kolasinski@poczta.fm>
2018-09-21 21:43:40 +0200

    Deletion scroll to convenient place after click in the circle

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -1,23 +1,12 @@
 $(document).ready( function() {
 
+    var movedToTop = $(".fas.fa-arrow-alt-circle-up");
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2 ) {
           $(".navigation-top").addClass("navigation-top-scrolled");
         } else {
-        $(".navigation-top").removeClass("navigation-top-scrolled");
+            $(".navigation-top").removeClass("navigation-top-scrolled");
         }
-
-        if ($(this).scrollTop() > 320 ) {
-            $(".fas.fa-arrow-alt-circle-up").show();
-        } else {
-            $(".fas.fa-arrow-alt-circle-up").hide();
-        }
-      });
-
-      $(".fas.fa-arrow-alt-circle-up").click(function() {
-          $('html').animate(
-              { scrollTop: 0}, 400, 'linear'
-          );
-      });
-
+    });
 });
