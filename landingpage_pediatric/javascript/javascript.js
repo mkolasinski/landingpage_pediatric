9747e7b8edb55ece11678c883d153a18d815e21f
mkolasinski <m.kolasinski@poczta.fm>
2018-09-21 21:10:25 +0200

    Add animate on click arrow

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -1,30 +1,23 @@
 $(document).ready( function() {
 
-    $('#about_me').on('click', function() {
-        window.scrollTo(0,590);
-        $(".mobile-menu").hide();
-        $(".far.fa-times-circle").removeClass("far fa-times-circle");
-    });
-
-    $('#home_visit').on('click', function() {
-        window.scrollTo(0,900);
-        $(".mobile-menu").hide();
-        $(".far.fa-times-circle").removeClass("far fa-times-circle");
-    });
-
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2 ) {
           $(".navigation-top").addClass("navigation-top-scrolled");
-          $(".mobile-menu").addClass("mobile-menu-scrolled");
         } else {
-          $(".navigation-top").removeClass("navigation-top-scrolled");
-          $(".mobile-menu").removeClass("mobile-menu-scrolled");
+        $(".navigation-top").removeClass("navigation-top-scrolled");
+        }
+
+        if ($(this).scrollTop() > 320 ) {
+            $(".fas.fa-arrow-alt-circle-up").show();
+        } else {
+            $(".fas.fa-arrow-alt-circle-up").hide();
         }
       });
 
-    $(".fas.fa-bars").on("click", function() {
-        $(this).toggleClass("far fa-times-circle");
-        $(".mobile-menu").toggle();
-    });
+      $(".fas.fa-arrow-alt-circle-up").click(function() {
+          $('html').animate(
+              { scrollTop: 0}, 400, 'linear'
+          );
+      });
 
 });
