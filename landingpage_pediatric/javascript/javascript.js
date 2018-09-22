f9be7f662b0784b8513c126daea4cf2e57e9506b
mkolasinski <m.kolasinski@poczta.fm>
2018-09-22 13:47:40 +0200

    New photo and trying to add quotation

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -1,7 +1,4 @@
 $(document).ready( function() {
-
-    var movedToTop = $(".fas.fa-arrow-alt-circle-up");
-
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2 ) {
           $(".navigation-top").addClass("navigation-top-scrolled");
