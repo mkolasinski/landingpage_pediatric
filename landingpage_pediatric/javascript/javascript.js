1d7fb188922b14c077e54972c4d1f3bbeeb0780e
mkolasinski <m.kolasinski@poczta.fm>
2018-11-01 20:14:59 +0100

    Adding google analytics?

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -1,4 +1,10 @@
 $(document).ready( function() {
+    
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128515459-1');
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2 ) {
           $(".navigation-top").addClass("navigation-top-scrolled");
