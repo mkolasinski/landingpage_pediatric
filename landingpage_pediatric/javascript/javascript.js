bfa5dbc80651253875812c61a9780ffb2ba3e1d9
mkolasinski <m.kolasinski@poczta.fm>
2019-03-09 18:34:16 +0100

    New version

--- landingpage_pediatric/javascript/javascript.js
+++ landingpage_pediatric/javascript/javascript.js
@@ -11,4 +11,17 @@ $(document).ready( function() {
             $(".navigation-top").removeClass("navigation-top-scrolled");
         }
     });
+
+    // based polish version
+    $(".fas.fa-angle-down").on("click", function() {
+        $(this).hide(300);
+        $(".fas.fa-angle-up").show(300);
+        $(".nsk").show();
+    });
+
+    $(".fas.fa-angle-up").on("click", function() {
+        $(this).hide(300);
+        $(".fas.fa-angle-down").show(300);
+        $(".nsk").hide(300);
+    });
 });
